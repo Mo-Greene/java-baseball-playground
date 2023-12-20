bc0146e4b31a4c00a4d4d7e43ba9ed90c206daf2
MoGreene <13blueboy13@naver.com>
2023-12-20 16:22:53 +0900

    input 코드 작성

--- src/test/java/study/StringCalcTest.java
+++ src/test/java/study/StringCalcTest.java
@@ -1,7 +1,6 @@
 package study;
 
 import java.io.ByteArrayInputStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,15 +11,23 @@ public class StringCalcTest {
 
 	@BeforeEach
 	public void setUp() {
-		String polynomial = "2 / 2 * 100 / 50";
+		String polynomial = "2 / 2 * 100 - 50 + 20";
 		System.setIn(new ByteArrayInputStream(polynomial.getBytes()));
 	}
 
 	@Test
 	@DisplayName("문자열 계산기")
 	public void stringCalcTest() {
+		String output = setString();
+		String[] values = output.split(" ");
+		int result = Integer.parseInt(values[0]);
+		for (int i = 1; i <= values.length; i++) {
+
+		}
+	}
+
+	private String setString() {
 		Scanner sc = new Scanner(System.in);
-		String out = sc.nextLine();
-		String[] values = out.split(" ");
+		return sc.nextLine();
 	}
 }
